6fb711634fb1c2303d703cd627e94b397ec55e5b
Takumi Sakai <tnsakai1996@gmail.com>
2025-06-19 15:34:36 -0700

    Edit comments

--- handler_help.go
+++ handler_help.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 )
 
-// handlerReset deletes all users from the database.
 func handlerHelp(s *state, cmd command) error {
 	fmt.Println("For list of commands, refer to https://github.com/TakuumiS/blogAgg")
 	return nil
